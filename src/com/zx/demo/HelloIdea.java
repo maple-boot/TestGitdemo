4d0ff9b4c4dda1a78457c7e0f22f567524515f80
maple-boot <18755149061@163.com>
2019-08-09 16:54:04 +0800

    maple commit

---
+++ src/com/zx/demo/HelloIdea.java
@@ -0,0 +1,7 @@
+package com.zx.demo;
+
+public class HelloIdea {
+    public static void main(String[] args) {
+        System.out.println("helfls");
+    }
+}
--- src/com/zx/demo/HelloWorld.java
+++ src/com/zx/demo/HelloWorld.java
@@ -2,6 +2,7 @@ package com.zx.demo;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("fdbsd");
         System.out.print("HelloWorld");
     }
 }
